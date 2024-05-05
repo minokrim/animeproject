39e13adccb7eb1282ff3d1693244ac7b6e55965a
ayomide <ayomidekareem563@gmail.com>
2024-05-05 12:13:33 -0700

    original

--- src/bookmark/bookmarkmain.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Bookmarksection from "./bookmark";
-function BookmarMain(){
-    return <div>
-        <Bookmarksection/>
-
-    </div>
-}
-export default BookmarMain
