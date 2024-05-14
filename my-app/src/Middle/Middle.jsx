1869e635f7add8544ec34c9ba9260bb8d3f156bf
ayomide <ayomidekareem563@gmail.com>
2024-05-13 17:00:16 -0700

    middle links

--- my-app/src/Middle/Middle.jsx
+++ my-app/src/Middle/Middle.jsx
@@ -8,10 +8,12 @@ return(
 <div className="container-fluid justify-content-center align-items-center">
     <div className="costum-div costum-bg  d-flex align-items-center">
     <div className="costum-div2  d-flex  justify-content-center  ">
-        <a className="link-costum p-0  " href="/animeproject">Newest</a>
-       
-        <a className="link-costum  p-0 " href="/popular">Popular</a>
-        
+        <Link to="/animeproject">
+        <a className="link-costum p-0  " href="">Newest</a>
+        </Link>
+       <Link to="/popular">
+       <a className="link-costum  p-0 " href="">Popular</a>
+       </Link>
     </div>
     
     </div>
